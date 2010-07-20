93347a11ae87cba3f7899213be01f1fce2bcc908
georgen@google.com <georgen@google.com@5c829b59-20ba-c297-566c-b9ce016b2d6a>
2010-07-20 22:22:13 +0000

    Initial fork of iTerm

---
+++ PTYWindow.h
@@ -0,0 +1,43 @@
+/* -*- mode:objc -*- */
+/* $Id: PTYWindow.h,v 1.2 2003-02-12 07:52:47 ujwal Exp $ */
+/* Incorporated into iTerm.app by Ujwal S. Sathyam */
+/*
+ **  PTYWindow.h
+ **
+ **  Copyright (c) 2002, 2003
+ **
+ **  Author: Fabian, Ujwal S. Sathyam
+ **	     Initial code by Kiichi Kusama
+ **
+ **  Project: iTerm
+ **
+ **  Description: NSWindow subclass. Implements transparency.
+ **
+ **  This program is free software; you can redistribute it and/or modify
+ **  it under the terms of the GNU General Public License as published by
+ **  the Free Software Foundation; either version 2 of the License, or
+ **  (at your option) any later version.
+ **
+ **  This program is distributed in the hope that it will be useful,
+ **  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ **  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ **  GNU General Public License for more details.
+ **
+ **  You should have received a copy of the GNU General Public License
+ **  along with this program; if not, write to the Free Software
+ **  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+
+#import <Cocoa/Cocoa.h>
+
+@interface PTYWindow : NSWindow 
+{
+}
+
+- (float)_transparency;
+- initWithContentRect:(NSRect)contentRect 
+            styleMask:(unsigned int)aStyle 
+	      backing:(NSBackingStoreType)bufferingType 
+		defer:(BOOL)flag;
+@end
